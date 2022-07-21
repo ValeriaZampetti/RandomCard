5e168a6687d825c92768c76ac5539ccef42eee54
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-07-21 03:24:20 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function CardGenerator() {
+  let numero = document.querySelector("#numero");
+  let pinta1 = document.querySelector("#pinta-1");
+  let pinta2 = document.querySelector("#pinta-2");
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  numero.innerHTML = numeros[Math.floor(Math.random() * 13)];
 
+  let pintas = ["♦", "♥", "♣", "♠"];
+  let pinta = pintas[Math.floor(Math.random() * 4)];
+  pinta1.innerHTML = pinta;
+  pinta2.innerHTML = pinta;
+  pinta2.style.transform = "rotate(180deg)";
+
+  if (pinta === "♦" || pinta === "♥") {
+    pinta1.style.color = "red";
+    pinta2.style.color = "red";
+  } else {
+    pinta1.style.color = "dark";
+    pinta2.style.color = "dark";
+  }
+}
+console.log(numero);
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  CardGenerator();
 };
